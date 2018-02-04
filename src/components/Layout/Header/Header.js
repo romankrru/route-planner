c6cd6023cb76d82e7547b7258aa1fd8fa72da1a2
Roman <rm07ru@gmail.com>
2018-02-04 18:54:29 +0300

    Add header

---
+++ src/components/Layout/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './Header.css';
+
+const Header = () => {
+  return (
+    <div className={styles.Header}>
+      ROUTE PLANNER
+    </div>
+  );
+};
+
+export default Header;
